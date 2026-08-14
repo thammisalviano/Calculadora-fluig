48da0f5c1fd43e4d25aff1defd21c2bf710ad13a
thammisalviano <84717442+thammisalviano@users.noreply.github.com>
2026-08-14 14:17:58 -0300

    Revise service worker to include additional assets
    Updated service worker to version 3 and added new assets for caching.

--- sw.js
+++ sw.js
@@ -1,4 +1,33 @@
-const CACHE='fluig-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='fluig-v3';
+
+const ASSETS=[
+ './',
+ './index.html',
+ './manifest.json',
+ './sw.js',
+ './precos_atuais.json'
+];
+
+self.addEventListener('install',e=>
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  )
+);
+
+self.addEventListener('activate',e=>
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys
+          .filter(k=>k!==CACHE)
+          .map(k=>caches.delete(k))
+      )
+    )
+  )
+);
+
+self.addEventListener('fetch',e=>
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  )
+);
